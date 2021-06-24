0251d0bfc5d631f36174965a0b4e7ef912ad7408
ArielXT <68018009+ArielXT@users.noreply.github.com>
2021-06-23 21:55:34 -0500

    Add files via upload

---
+++ app/Http/Middleware/SoloAdmin.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SoloAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(auth::user()->fullacces=='si'):
+            return $next($request);
+        endif;
+            return redirect('usuario');
+    }
+}
